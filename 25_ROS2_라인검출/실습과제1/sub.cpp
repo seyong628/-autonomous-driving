d38445fcb145cafae3b099a4a7ea7d6ed6f46e67
seyong628 <seyong7521@naver.com>
2025-12-03 15:41:03 +0900

    Update sub.cpp

--- 25_ROS2_라인검출/실습과제1/sub.cpp
+++ 25_ROS2_라인검출/실습과제1/sub.cpp
@@ -12,10 +12,25 @@ cv::VideoWriter recorder("save.mp4",cv::VideoWriter::fourcc('X','2','6','4'),10,
 void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
 {
     auto startTime = std::chrono::steady_clock::now();
+    cv::Mat gray, bin, frame2, ;
     cv::Mat frame = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
-
     recorder.write(frame);
+    cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
+    frame2 = gray + (100.0 - cv::mean(gray)[0]);
+    cv::threshold(gray,bin,120,255,cv::THRESH_BINARY);
+    cv::Mat roi = bin(cv::Rect(0,270,640,90));
+    cv::Mat labels, stats, centroids;
+    int num = cv::connectedComponentsWithStats(roi, labels, stats, centroids);
+    int max_area = 0, max_label = -1;
+    for(int i = 1; i < num; i++){
+        int area = stats.at<int>(i, cv::)
+    }
+    
+    //cv::imshow("gray",gray);
+    //cv::imshow("frame2",frame2);
     cv::imshow("wsl",frame);
+    cv::imshow("wsl2",roi);
+    
     cv::waitKey(1);
     RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
     auto endTime = std::chrono::steady_clock::now(); // 종료시간 저장
